4b315a0166f177b64d34db4df6aaebc2b0c978c5
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-23 20:14:34 +0900

    add assets files and duplicate applicatoincontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :set_layout
+
+  private
+
+  def set_layout
+    if params[:controller] =~ /\A(staff|admin|customer)/
+      $1
+    else
+      'customer'
+    end
+  end
 end
