76c2095a56cc07d7e4fe735bf6a331d5f430a893
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-24 01:26:52 +0900

    remove null construction for end_date

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20171023154656) do
     t.string   "given_name_kana",                  null: false
     t.string   "hashed_password"
     t.date     "start_date",                       null: false
-    t.date     "end_date",                         null: false
+    t.date     "end_date"
     t.boolean  "suspended",        default: false, null: false
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
