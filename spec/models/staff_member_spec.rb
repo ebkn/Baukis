6f769a6bf12785c264e35e25fbc2b7ec0572790f
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-24 01:08:22 +0900

    add test for password= method

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#password=' do
+    it 'make hashed_password 60 characters' do
+      member = StaffMember.new
+      member.password = 'baukis'
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+  end
 end
