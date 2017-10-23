17ae41f0ddc7f3d3dc0d17ffc108d99b4dfd1b98
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-24 00:38:05 +0900

    delete errors_controller_spec cause by automatically created

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe ErrorsController, type: :controller do
 
-  describe "GET #routing_error" do
-    it "returns http success" do
-      get :routing_error
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
