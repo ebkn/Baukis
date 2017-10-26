075d6099d7711ce6b1579edd8d34b82d28e25910
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-27 00:58:07 +0900

    create and rename password= method spec

--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Administrator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#password=" do
+    it 'creates 60 characters hashed_password' do
+      administrator = Administrator.new
+      administrator.password = Faker::StarWars.character
+      expect(administrator.hashed_password).to be_kind_of(String)
+      expect(administrator.hashed_password.size).to eq(60)
+    end
+
+    it 'creates nil hashed_password' do
+      administrator = Administrator.new
+      administrator.password = nil
+      expect(administrator.hashed_password).to be_nil
+    end
+  end
 end
