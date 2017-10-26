9a17b2c2bf7588c6ce98185c172dd1d98b204701
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-27 01:07:51 +0900

    add session routing to admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    get 'login' => 'sessions#new', as: :login
+    post 'session' => 'sessions#create', as: :session
+    delete 'session' => 'sessions#destroy'
     root 'top#index'
   end
 
