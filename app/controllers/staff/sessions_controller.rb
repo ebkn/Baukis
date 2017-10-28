7785c73512074398ab3ece2947e4fd241cb5ce7b
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-29 00:14:23 +0900

    create create, destroy action

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Staff::SessionsController < ApplicationController
+class Staff::SessionsController < Staff::Base
   def new
     if current_staff_member
       redirect_to :staff_root
@@ -6,5 +6,29 @@ class Staff::SessionsController < ApplicationController
       @form = Staff::LoginForm.new
       render :new
     end
+  end
+
+  def create
+    @form = Staff::LoginForm.new(login_form_params)
+    if @form.email.present?
+      staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
+    end
+    if staff_member
+      session[:staff_member_id] = staff_member.id
+      redirect_to staff_root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:staff_member_id)
+    redirect_to staff_root_path
+  end
+
+  private
+
+  def login_form_params
+    params.require(:staff_login_form).permit(:email, :password)
   end
 end
