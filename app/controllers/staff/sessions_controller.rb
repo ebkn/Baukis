dd463f9a7732239ca277af3ab1924670328446c9
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-31 01:34:06 +0900

    add suspend flash

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -14,15 +14,23 @@ class Staff::SessionsController < Staff::Base
 
   def create
     @form = Staff::LoginForm.new(login_form_params)
-    if @form.email.present?
+
+    if form_filled?(@form)
       staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
-    end
-    if Staff::Authenticator.new(staff_member).authenticate(@form.password)
-      session[:staff_member_id] = staff_member.id
-      flash.notice = 'ログインしました'
-      redirect_to staff_root_path
+
+      if staff_member.suspended
+        flash.now.alert = 'アカウントが凍結されています'
+        render :new
+      elsif Staff::Authenticator.new(staff_member).authenticate(@form.password)
+        session[:staff_member_id] = staff_member.id
+        flash.notice = 'ログインしました'
+        redirect_to staff_root_path
+      else
+        flash.now.alert = 'メールアドレスまたはパスワードが間違っています'
+        render :new
+      end
     else
-      flash.now.alert = 'メールアドレスまたはパスワードが間違っています'
+      flash.now.alert = 'メールアドレスとパスワードを入力してください'
       render :new
     end
   end
@@ -37,5 +45,9 @@ class Staff::SessionsController < Staff::Base
 
   def login_form_params
     params.require(:staff_login_form).permit(:email, :password)
+  end
+
+  def form_filled?(form_data)
+    form_data.email.present? && form_data.password.present?
   end
 end
