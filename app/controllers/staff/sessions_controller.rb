79365ab736f5eddb57664897b72057e70eee7209
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-31 19:24:15 +0900

    create sessions_controller spec coveraged 100%

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -5,7 +5,7 @@ class Staff::SessionsController < Staff::Base
 
   def new
     if current_staff_member
-      redirect_to :staff_root
+      redirect_to staff_root_path
     else
       @form = Staff::LoginForm.new
     end
@@ -23,17 +23,18 @@ class Staff::SessionsController < Staff::Base
     staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
 
     if staff_member.nil?
-      flash.now.alert = 'メールアドレスまたはパスワードが間違っています'
-      render :new
-    elsif staff_member.suspended
-      flash.now.alert = 'アカウントが凍結されています'
+      flash.now.alert = 'メールアドレスが間違っています'
       render :new
     elsif Staff::Authenticator.new(staff_member).authenticate(@form.password)
       session[:staff_member_id] = staff_member.id
       flash.notice = 'ログインしました'
       redirect_to staff_root_path
+    elsif staff_member.suspended?
+      flash.now.alert = 'アカウントが凍結されています'
+      render :new
     else
-      redirect_to staff_login_path, alert: 'エラーが発生しました'
+      flash.now.alert = 'パスワードが間違っています'
+      render :new
     end
   end
 
