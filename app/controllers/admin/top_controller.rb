5b2d9e6b7534be0d8519a9d3e1c66a54843807f0
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-01 00:25:23 +0900

    create admin/staff_members routing and dashboard display

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,4 +1,5 @@
 class Admin::TopController < Admin::Base
   def index
+    render :dashboard if current_administrator
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :staff do
+  namespace :staff, path: '/' do
     get 'login' => 'sessions#new', as: :login
     get 'session' => 'sessions#index', as: :session_failed
     post 'session' => 'sessions#create', as: :session
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     get 'session' => 'sessions#index', as: :session_failed
     post 'session' => 'sessions#create', as: :session
     delete 'session' => 'sessions#destroy'
+    resources :staff_members
     root 'top#index'
   end
 
@@ -19,6 +20,5 @@ Rails.application.routes.draw do
     root 'top#index'
   end
 
-  root 'customer/top#index'
   get '*anything' => 'errors#routing_error'
 end
