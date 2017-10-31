d5bc12947d95ece26196c8e8bcbf3cb6f92f661d
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-01 02:02:43 +0900

    create constraints of host and path

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,30 @@
 Rails.application.routes.draw do
-  namespace :staff, path: '/' do
-    get 'login' => 'sessions#new', as: :login
-    get 'session', to: redirect('/login')
-    resource :session, only: %i[create destroy]
-    resource :account, only: %i[index edit update]
-    root 'top#index'
+  config = Rails.application.config.baukis
+
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      get 'login' => 'sessions#new', as: :login
+      get 'session', to: redirect('/login')
+      resource :session, only: %i[create destroy]
+      resource :account, only: %i[show edit update]
+      root 'top#index'
+    end
   end
 
-  namespace :admin do
-    get 'login' => 'sessions#new', as: :login
-    get 'session', to: redirect('/admin/login')
-    resource :session, only: %i[create destroy]
-    resources :staff_members
-    root 'top#index'
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path] do
+      get 'login' => 'sessions#new', as: :login
+      get 'session', to: redirect('/admin/login')
+      resource :session, only: %i[create destroy]
+      resources :staff_members
+      root 'top#index'
+    end
   end
 
-  namespace :customer do
-    root 'top#index'
+  constraints host: config[:customer][:host] do
+    namespace :customer, path: config[:customer][:path] do
+      root 'top#index'
+    end
   end
 
   get '*anything' => 'errors#routing_error'
