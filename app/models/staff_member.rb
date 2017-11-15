31c1e382214309c54d7c5caf8aa86cd650a44c0e
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-16 00:43:00 +0900

    modify indent

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -6,23 +6,23 @@ class StaffMember < ApplicationRecord
   default_scope { order(:family_name_kana, :given_name_kana) }
 
   before_validation do
-    self.email = normalize_as_email(email)
-    self.email_for_index = email.downcase if email
-    self.family_name = normalize_as_name(family_name)
-    self.given_name = normalizer_as_name(given_name)
+    self.email            = normalize_as_email(email)
+    self.email_for_index  = email.downcase if email
+    self.family_name      = normalize_as_name(family_name)
+    self.given_name       = normalizer_as_name(given_name)
     self.family_name_kana = normalize_as_furigana(family_name_kana)
-    self.given_name_kana = normalize_as_furigana(given_name_kana)
+    self.given_name_kana  = normalize_as_furigana(given_name_kana)
   end
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   KATAKANA_REGEX = /\A[\p{katakana}\u{30fc}]\z/
 
   validates :email, presence: true,
-    format: { with: VALID_EMAIL_REGEX, allow_blank: true }
+                    format: { with: VALID_EMAIL_REGEX, allow_blank: true }
   validates :email_for_index, uniqueness: true
   validates :family_name, :given_name, presence: true
   validates :family_name_kana, :given_name_kana, presence: true,
-    format: { with: KATAKANA_REGEX, allow_blank: true }
+                                                 format: { with: KATAKANA_REGEX, allow_blank: true }
   validates :start_date, presence: true, date: {
     after_or_equal_to: Date.new(2017, 1, 1),
     before: -> { 1.year.from_now.to_date },
