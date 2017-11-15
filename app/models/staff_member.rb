eb93828d6082ce650571bdc3df08e6491ce33a36
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-16 00:37:55 +0900

    add email validation

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -6,6 +6,7 @@ class StaffMember < ApplicationRecord
   default_scope { order(:family_name_kana, :given_name_kana) }
 
   before_validation do
+    self.email = normalize_as_email(email)
     self.email_for_index = email.downcase if email
     self.family_name = normalize_as_name(family_name)
     self.given_name = normalizer_as_name(given_name)
@@ -13,16 +14,27 @@ class StaffMember < ApplicationRecord
     self.given_name_kana = normalize_as_furigana(given_name_kana)
   end
 
-  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]\z/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  KATAKANA_REGEX = /\A[\p{katakana}\u{30fc}]\z/
 
+  validates :email, presence: true,
+    format: { with: VALID_EMAIL_REGEX, allow_blank: true }
+  validates :email_for_index, uniqueness: true
   validates :family_name, :given_name, presence: true
   validates :family_name_kana, :given_name_kana, presence: true,
-    format: { with: KATAKANA_REGEXP, allow_blank: true }
+    format: { with: KATAKANA_REGEX, allow_blank: true }
   validates :start_date, presence: true, date: {
     after_or_equal_to: Date.new(2017, 1, 1),
     before: -> { 1.year.from_now.to_date },
     allow_blank: true
   }
+
+  after_validation do
+    if errors.include?(:email_for_index)
+      errors.add(:email, :token)
+      errors.delete(:email_for_index)
+    end
+  end
 
   def password=(raw_password)
     if raw_password.is_a?(String)
