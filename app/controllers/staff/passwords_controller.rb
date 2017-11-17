2ff0c2a899f4119b1d65c0709a7b9f4a05057b4c
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-17 22:05:02 +0900

    add validation and create actions

--- app/controllers/staff/passwords_controller.rb
+++ app/controllers/staff/passwords_controller.rb
@@ -1,5 +1,25 @@
-class Staff::PasswordsController < ApplicationController
+class Staff::PasswordsController < Staff::Base
   def show
     redirect_to edit_staff_password_path
+  end
+
+  def edit
+    @change_password_form =
+      Staff::ChangePasswordForm.new(object: current_staff_member)
+  end
+
+  def update
+    @change_password_form =
+      Staff::ChangePasswordForm.new(staff_change_password_params)
+  end
+
+  private
+
+  def staff_change_password_params
+    params.require(:staff_change_password_form).permit(
+      :current_password,
+      :new_password,
+      :new_password_confirmation
+    )
   end
 end
--- app/form/staff/change_password_form.rb
+++ app/form/staff/change_password_form.rb
@@ -6,7 +6,17 @@ class Staff::ChangePasswordForm
                 :new_password,
                 :new_password_confirmation
 
+  validates :current_password, presence: true
+  validates :new_password, presence: true, confirmation: true
+
+  validate do
+    unless Staff::Authenticator.new(object).authenticate(:current_password)
+      errors.add(:current_password, :wrong)
+    end
+  end
+
   def save
+    return unless valid?
     object.password = new_password
     object.save!
   end
