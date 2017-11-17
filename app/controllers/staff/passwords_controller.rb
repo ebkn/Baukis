99bf3e13a4b6babf6f10ab06198c114d72d5db5f
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-17 23:08:34 +0900

    add update function and flash errors

--- app/controllers/staff/passwords_controller.rb
+++ app/controllers/staff/passwords_controller.rb
@@ -11,6 +11,13 @@ class Staff::PasswordsController < Staff::Base
   def update
     @change_password_form =
       Staff::ChangePasswordForm.new(staff_change_password_params)
+    @change_password_form.object = current_staff_member
+    if @change_password_form.save
+      redirect_to staff_member_path, notice: 'パスワードを変更しました'
+    else
+      flash.now.alert = '入力に誤りがあります'
+      render :edit
+    end
   end
 
   private
