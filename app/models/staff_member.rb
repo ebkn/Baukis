32c9383951e500c7d486fb3c50bddea1ea730794
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-17 22:34:24 +0900

    add name regex

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -14,15 +14,17 @@ class StaffMember < ApplicationRecord
     self.given_name_kana  = normalize_as_furigana(given_name_kana)
   end
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
-  KATAKANA_REGEX = /\A[\p{katakana}\u{30fc}]+\z/
+  VALID_EMAIL_REGEX    = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  VALID_NAME_REGEX     = /\A[\p{han}\p{hiragana}\p{katakana}\u{30fc}A-Za-z]+\z/
+  VALID_KATAKANA_REGEX = /\A[\p{katakana}\u{30fc}]+\z/
 
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX, allow_blank: true }
   validates :email_for_index, uniqueness: true
-  validates :family_name, :given_name, presence: true
+  validates :family_name, :given_name, presence: true,
+                                       format: { with: VALID_NAME_REGEX, allow_blank: true }
   validates :family_name_kana, :given_name_kana, presence: true,
-                                                 format: { with: KATAKANA_REGEX, allow_blank: true }
+                                                 format: { with: VALID_KATAKANA_REGEX, allow_blank: true }
   validates :start_date, presence: true, date: {
     after_or_equal_to: Date.new(2017, 1, 1),
     before: proc { 1.year.from_now.to_date },
