f76ba3dfd74dee38c443dee4dbf91f7f66fb9075
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-18 17:41:23 +0900

    create and modify javascripts folder and load datas

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,3 @@
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
-Rails.application.config.assets.precompile += %w[admin.css staff.css customer.css]
+Rails.application.config.assets.precompile +=
+  %w[admin.css staff.css customer.css admin.js staff.js customer.js]
