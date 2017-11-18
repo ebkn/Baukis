01c705334ba0245760e1355e700346bc72f3834a
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-18 16:19:38 +0900

    create view using presenter

--- app/presents/staff_member_form_presenter.rb
+++ app/presents/staff_member_form_presenter.rb
@@ -1,4 +1,8 @@
 class StaffMemberFormPresenter < FormPresenter
+  def password_field_block(name, label_text, options = {})
+    super(name, label_text, options) if object.new_record?
+  end
+
   def full_name_block(name1, name2, label_text, options = {})
     markup(:div, class: 'generic_form-input') do |m|
       m << label(
@@ -6,8 +10,17 @@ class StaffMemberFormPresenter < FormPresenter
         label_text,
         class: options[:required] ? 'required' : nil
       )
+      options[:placeholder] = 'Family name'
       m << text_field(name1, options)
+      options[:placeholder] = 'Given name'
       m << text_field(name2, options)
+    end
+  end
+
+  def check_box_block
+    markup(:div, class: 'generic_form-input') do |m|
+      m << check_box(:suspended)
+      m << label(:suspend, 'アカウント停止')
     end
   end
 end
