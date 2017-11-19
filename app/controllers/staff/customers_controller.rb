9b3163102ad6ad0e2a4d449b5d74291b2870d621
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-19 20:27:15 +0900

    add create/update/destroy action about customers

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -11,7 +11,35 @@ class Staff::CustomersController < Staff::Base
     @customer_form = Staff::CustomerForm.new
   end
 
+  def create
+    @customer_form = Staff::CustomerForm.new
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.save
+      redirect_to staff_customers_path, notice: '顧客を追加しました'
+    else
+      flash.now.alert = '入力に誤りがあります'
+      render :new
+    end
+  end
+
   def edit
     @customer_form = Staff::CustomerForm.new(Customer.find(params[:id]))
+  end
+
+  def update
+    @customer_form = Staff::CustomerForm.new(Customer.find(params[:id]))
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.save
+      redirect_to staff_customers_path, notice: '顧客情報を更新しました'
+    else
+      flash.now.alert = '入力に誤りがあります'
+      render :edit
+    end
+  end
+
+  def destroy
+    customer = Customer.find(params[:id])
+    customer.destroy!
+    redirect_to staff_customers_path, notice: '顧客アカウントを削除しました'
   end
 end
--- app/form/staff/customer_form.rb
+++ app/form/staff/customer_form.rb
@@ -6,7 +6,7 @@ class Staff::CustomerForm
 
   def initialize(customer = nil)
     @customer = customer
-    @customer ||= Customer.new()
+    @customer ||= Customer.new
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address
   end
@@ -14,8 +14,9 @@ class Staff::CustomerForm
   def assign_attributes(params = {})
     @params = params
 
-    customer.assgin_attributes(home_address_params)
-    customer.assign_attributes(work_address_params)
+    customer.assign_attributes(customer_params)
+    customer.home_address.assign_attributes(home_address_params)
+    customer.work_address.assign_attributes(work_address_params)
   end
 
   def save
