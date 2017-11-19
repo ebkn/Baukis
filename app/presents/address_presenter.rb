caae7c76c00b793cf6e8312a87e22a560c00aebd
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-19 16:05:17 +0900

    create staff/customer#show screen

--- app/presents/address_presenter.rb
+++ app/presents/address_presenter.rb
@@ -1,12 +1,4 @@
 class AddressPresenter < ModelPresenter
-  delegate :prefecture,
-           :city,
-           :address1,
-           :address2,
-           :campany_name,
-           :division_name,
-           to: :object
-
   def postal_code
     if md = object.postal_code.match(/\A(\d{3})(\d{4})\z/)
       md[1] + '-' + md[2]
