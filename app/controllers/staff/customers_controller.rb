7ce4e2d663e3704b1adffdb022ba120a76a6d1e9
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-19 14:53:28 +0900

    create address/presenter and show action

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -2,4 +2,8 @@ class Staff::CustomersController < Staff::Base
   def index
     @customers = Customer.page(params[:page])
   end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
 end
