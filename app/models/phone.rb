307732679ae465306d22d0f52add7dccac616d9a
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-29 16:16:42 +0900

    add validation to phone model

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,2 +1,18 @@
 class Phone < ApplicationRecord
+  include StringNormalizer
+
+  belongs_to :customer
+  belongs_to :address
+
+  before_validation do
+    self.number = normalize_as_phone_number
+    self.number_for_index = number.gsub(/\D/, '') if number
+  end
+
+  before_create { self.customer = address.customer if address }
+
+  VALID_PHONE_NUMBER = /\A\+?\d+(-\d+)*\z/
+
+  validates :number, presence: true,
+                     format: { with: VALID_PHONE_NUMBER, allow_blank: true }
 end
