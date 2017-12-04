0370e6fb51f56f2cd772c1d1c96d9263f7034b85
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-05 00:06:38 +0900

    delete bootstrap gem and add bootstrap asset files

--- app/assets/javascripts/staff.js
+++ app/assets/javascripts/staff.js
@@ -1,7 +1,6 @@
 //= require jquery3
 //= require jquery_ujs
-//= require popper
-//= require bootstrap
+//= require_tree ./vendor/
 //= require turbolinks
 //= require jquery-ui/widgets/datepicker
 //= require jquery-ui/i18n/datepicker-ja
