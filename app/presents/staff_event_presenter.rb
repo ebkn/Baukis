c3bf12751e42d08b4a422b3885ad340849727a16
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-05 17:44:45 +0900

    create login/logout record screen

--- app/presents/staff_event_presenter.rb
+++ app/presents/staff_event_presenter.rb
@@ -1,12 +1,14 @@
 class StaffEventPresenter < ModelPresenter
+  include Rails.application.routes.url_helpers
+
   delegate :member, :description, :occured_at, to: :object
 
   def table_row
-    markup(:tr) do |m|
+    markup(:tr, class: 'text-center') do |m|
       unless view_context.instance_variable_get(:@staff_member)
         m.td do
           m << link_to(
-            member.full_name,
+            full_name(member),
             admin_staff_member_staff_events_path(member)
           )
         end
@@ -14,5 +16,11 @@ class StaffEventPresenter < ModelPresenter
       m.td description
       m.td occured_at.strftime('%Y/%m/%d %H:%M:%S')
     end
+  end
+
+  private
+
+  def full_name(member)
+    "#{member.family_name} #{member.given_name}"
   end
 end
