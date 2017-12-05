f232ec325cd9444a0720c748a88fc7e866f93c3b
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-05 16:43:31 +0900

    create admin/staff_member edit screen

--- app/presents/user_form_presenter.rb
+++ app/presents/user_form_presenter.rb
@@ -5,9 +5,21 @@ class UserFormPresenter < FormPresenter
     else
       markup(:div, class: 'form-group py-2') do |m|
         m << decorated_label(name, label_text, options.merge(required: false))
-        m.button('変更する', type: 'button', id: 'enable-password_field-button')
-        m.button('変更しない', type: 'button', id: 'disable-password_field-button')
-        m << password_field(name, options.merge(disabled: true))
+        m.button('変更する',
+                 type: 'button',
+                 class: 'btn btn-raised btn-warning mx-3',
+                 id: 'enable-password_field-button')
+        m.button('変更しない',
+                 type: 'button',
+                 id: 'disable-password_field-button',
+                 class: 'btn btn-raised btn-primary mx-3',
+                 style: 'display: none')
+        m << password_field(
+          name,
+          options.merge(disabled: true,
+                        class: 'form-control',
+                        style: 'display: none;')
+        )
         m << error_messages_for(name)
       end
     end
@@ -22,16 +34,16 @@ class UserFormPresenter < FormPresenter
             name1,
             options.merge(class: 'form-control', placeholder: 'Family name')
           )
+          m << error_messages_for(name1)
         end
         m.div(class: 'col') do
           m << text_field(
             name2,
             options.merge(class: 'form-control', placeholder: 'Given name')
           )
+          m << error_messages_for(name2)
         end
       end
-      m << error_messages_for(name1)
-      m << error_messages_for(name2)
     end
   end
 end
