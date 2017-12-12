016864db06aa880ab2bddcf2acf23569c6056286
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-12 16:19:36 +0900

    create staff/customer edit screen

--- app/presents/address_form_presenter.rb
+++ app/presents/address_form_presenter.rb
@@ -1,8 +1,7 @@
 class AddressFormPresenter < FormPresenter
   def postal_code_block(name, label_text, options)
     markup(:div, class: 'form-group py-2') do |m|
-      m << decorated_label(name, label_text, options)
-      m << text_field(name, options)
+      m << text_field_block(name, options)
       m.div '7桁以上の半角数字で入力してください', class: 'notes'
       m << error_messages_for(name)
     end
