8a1643bef4185f2e6f634b6535dff60556918d3a
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-14 18:39:09 +0900

    add include? method and spec

--- app/models/allowed_source.rb
+++ app/models/allowed_source.rb
@@ -9,6 +9,25 @@ class AllowedSource < ApplicationRecord
   validates :octet4,
             uniqueness: { scope: %i[octet1 octet2 octet3], allow_blank: true }
 
+  class << self
+    def include?(namespace, ip_address)
+      !Rails.application.config.baukis[:restrict_ip_addresses] ||
+        where(namespace: namespace).where(options_for(ip_address)).exists?
+    end
+
+    private
+
+    def options_for(ip_address)
+      octets = ip_address.split('.')
+      condition = <<-SQL
+        octet1 = ? AND octet2 = ? AND octet3 = ?
+        AND ((octet4 = ? AND wildcard = ?) OR  wildcard = ?)
+      SQL
+
+      [condition, *octets, false, true]
+    end
+  end
+
   def ip_address=(ip_address)
     octets = ip_address.split('.')
     self.octet1 = octets[0]
--- spec/models/allowed_source_spec.rb
+++ spec/models/allowed_source_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe AllowedSource, type: :model do
+  describe '.include?' do
+    before do
+      Rails.application.config.baukis[:restrict_ip_addresses] = true
+      AllowedSource.create!(namespace: 'staff', ip_address: '127.0.0.1')
+      AllowedSource.create!(namespace: 'staff', ip_address: '192.168.0.*')
+    end
+
+    context 'when ip address does not match' do
+      it 'returns false' do
+        expect(AllowedSource.include?('staff', '192.168.1.1')).to eq false
+      end
+    end
+
+    context 'when ip address matches' do
+      it 'returns true when all octets match' do
+        expect(AllowedSource.include?('staff', '127.0.0.1')).to eq true
+      end
+
+      it 'returns true when octets including wildcard match' do
+        expect(AllowedSource.include?('staff', '192.168.0.100')).to eq true
+      end
+    end
+  end
+
   describe '#ip_address=' do
     context 'when valid ip address' do
       it 'is valid when ip address is 127.0.0.1' do
