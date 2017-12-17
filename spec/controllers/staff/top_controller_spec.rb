76d2cc82059971cb639d8491782a553494a27f6e
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-17 23:10:21 +0900

    add staff/top_controller spec about ip address constraints

--- spec/controllers/staff/top_controller_spec.rb
+++ spec/controllers/staff/top_controller_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Staff::TopController, type: :controller do
+  let(:staff_member) { create(:staff_member) }
+
+  describe 'restrict by ip addresses' do
+    before { Rails.application.config.baukis[:restrict_ip_addresses] = true }
+
+    it 'allows to access' do
+      AllowedSource.create!(namespace: 'staff', ip_address: '0.0.0.0')
+      get :index
+      expect(response).to render_template 'staff/top/index'
+    end
+
+    it 'rejects to access' do
+      AllowedSource.create!(namespace: 'staff', ip_address: '192.168.0.*')
+      get :index
+      expect(response).to render_template 'errors/forbidden'
+    end
+  end
 end
