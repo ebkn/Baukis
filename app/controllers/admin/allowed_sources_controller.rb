1039018791b6c2f022c56b28e63cc7219aaf8565
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-19 14:54:25 +0900

    create new allowed source form

--- app/controllers/admin/allowed_sources_controller.rb
+++ app/controllers/admin/allowed_sources_controller.rb
@@ -2,5 +2,6 @@ class Admin::AllowedSourcesController < Admin::Base
   def index
     @allowed_sources = AllowedSource.order_by_octets
                                     .page(params[:page])
+    @new_allowed_source = AllowedSource.new
   end
 end
