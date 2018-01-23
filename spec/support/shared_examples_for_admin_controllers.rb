14497d0c4b6473fb12f932dd84e147078b33fb37
ebkn12 <ktennis.mqekr12@gmail.com>
2018-01-23 22:36:28 +0900

    refactoring shared_examples

--- spec/support/shared_examples_for_admin_controllers.rb
+++ spec/support/shared_examples_for_admin_controllers.rb
@@ -1,319 +1,141 @@
 shared_examples 'a administrator who does not login' do
+  let(:staff_member) { create(:staff_member) }
+  let(:staff_member_params) { attributes_for(:staff_member) }
+  subject do
+    expect(flash[:alert]).to eq 'ログインしてください'
+    expect(response).to redirect_to admin_login_path
+  end
+
   describe 'GET #index' do
     before { get :index }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    it { subject }
   end
 
   describe 'GET #new' do
     before { get :new }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    it { subject }
   end
 
   describe 'POST #create' do
-    let(:staff_member_params) { attributes_for(:staff_member) }
-
     before { post :create, params: { staff_member: staff_member_params } }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    it { subject }
   end
 
   describe 'GET #edit' do
-    before do
-      staff_member = create(:staff_member)
-      get :edit, params: { id: staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    before { get :edit, params: { id: staff_member } }
+    it { subject }
   end
 
   describe 'PATCH #update' do
-    let(:staff_member_params) { attributes_for(:staff_member) }
-
-    before do
-      create(:staff_member)
-      patch :update, params: { id: 1, staff_member: staff_member_params }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    before { patch :update, params: { id: staff_member, staff_member: staff_member_params } }
+    it { subject }
   end
 
   describe 'GET #show' do
-    before do
-      staff_member = create(:staff_member)
-      get :show, params: { id: staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    before { get :show, params: { id: staff_member } }
+    it { subject }
   end
 
   describe 'DELETE #destroy' do
-    before do
-      @staff_member = create(:staff_member)
-      delete :destroy, params: { id: @staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    before { delete :destroy, params: { id: staff_member } }
+    it { subject }
   end
 end
 
 shared_examples 'a administrator whose session is time out' do
   let(:administrator) { create(:administrator) }
-
+  let(:staff_member) { create(:staff_member) }
+  let(:staff_member_params) { attributes_for(:staff_member) }
   before do
     session[:administrator_id] = administrator.id
     session[:last_access_time] = Staff::Base::TIMEOUT.ago.advance(seconds: -1)
   end
+  subject do
+    expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
+    expect(response).to redirect_to admin_login_path
+  end
 
   describe 'GET #index' do
     before { get :index }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    it { subject }
   end
 
   describe 'GET #new' do
     before { get :new }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    it { subject }
   end
 
   describe 'POST #create' do
-    let(:staff_member_params) { attributes_for(:staff_member) }
-
     before { post :create, params: { staff_member: staff_member_params } }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    it { subject }
   end
 
   describe 'GET #edit' do
-    before do
-      staff_member = create(:staff_member)
-      get :edit, params: { id: staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    before { get :edit, params: { id: staff_member } }
+    it { subject }
   end
 
   describe 'PATCH #update' do
-    let(:staff_member_params) { attributes_for(:staff_member) }
-
-    before do
-      create(:staff_member)
-      patch :update, params: { id: 1, staff_member: staff_member_params }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    before { patch :update, params: { id: staff_member, staff_member: staff_member_params } }
+    it { subject }
   end
 
   describe 'GET #show' do
-    before do
-      staff_member = create(:staff_member)
-      get :show, params: { id: staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    before { get :show, params: { id: staff_member } }
+    it { subject }
   end
 
   describe 'DELETE #destroy' do
-    before do
-      @staff_member = create(:staff_member)
-      delete :destroy, params: { id: @staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_login_path
-    end
+    before { delete :destroy, params: { id: staff_member } }
+    it { subject }
   end
 end
 
 shared_examples 'a administrator who is suspended' do
   let(:administrator) { create(:administrator, suspended: true) }
-
+  let(:staff_member) { create(:staff_member) }
+  let(:staff_member_params) { attributes_for(:staff_member) }
   before do
     session[:administrator_id] = administrator.id
     session[:last_access_time] = 1.second.ago
   end
+  subject do
+    expect(flash[:alert]).to eq 'アカウントが無効です'
+    expect(response).to redirect_to admin_root_path
+  end
 
   describe 'GET #index' do
     before { get :index }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'アカウントが無効です'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_root_path
-    end
+    it { subject }
   end
 
   describe 'GET #new' do
     before { get :new }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'アカウントが無効です'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_root_path
-    end
+    it { subject }
   end
 
   describe 'POST #create' do
-    let(:staff_member_params) { attributes_for(:staff_member) }
-
     before { post :create, params: { staff_member: staff_member_params } }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'アカウントが無効です'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_root_path
-    end
+    it { subject }
   end
 
   describe 'GET #edit' do
-    before do
-      staff_member = create(:staff_member)
-      get :edit, params: { id: staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'アカウントが無効です'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_root_path
-    end
+    before { get :edit, params: { id: staff_member } }
+    it { subject }
   end
 
   describe 'PATCH #update' do
-    let(:staff_member_params) { attributes_for(:staff_member) }
-
-    before do
-      create(:staff_member)
-      patch :update, params: { id: 1, staff_member: staff_member_params }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'アカウントが無効です'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_root_path
-    end
+    before { patch :update, params: { id: staff_member, staff_member: staff_member_params } }
+    it { subject }
   end
 
   describe 'GET #show' do
-    before do
-      staff_member = create(:staff_member)
-      get :show, params: { id: staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'アカウントが無効です'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_root_path
-    end
+    before { get :show, params: { id: staff_member } }
+    it { subject }
   end
 
   describe 'DELETE #destroy' do
-    before do
-      @staff_member = create(:staff_member)
-      delete :destroy, params: { id: @staff_member }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'アカウントが無効です'
-    end
-
-    it 'redirects to admin_root_path' do
-      expect(response).to redirect_to admin_root_path
-    end
+    before { delete :destroy, params: { id: staff_member } }
+    it { subject }
   end
 end
--- spec/support/shared_examples_for_staff_controllers.rb
+++ spec/support/shared_examples_for_staff_controllers.rb
@@ -1,190 +1,100 @@
 shared_examples 'a staff_member who does not login' do
+  let(:staff_member_params) { attributes_for(:staff_member, email: 'test@test.com') }
+  subject do
+    expect(flash[:alert]).to eq 'ログインしてください'
+    expect(response).to redirect_to staff_login_path
+  end
+
   describe 'GET #show' do
     before { get :show }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to staff_root_path' do
-      expect(response).to redirect_to staff_login_path
-    end
+    it { subject }
   end
 
   describe 'GET #edit' do
     before { get :edit }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to staff_root_path' do
-      expect(response).to redirect_to staff_login_path
-    end
+    it { subject }
   end
 
   describe 'PATCH #update' do
-    let(:staff_member_params) { attributes_for(:staff_member, email: 'test@test.com') }
-
-    before do
-      patch :update, params: { staff_member: staff_member_params }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'ログインしてください'
-    end
-
-    it 'redirects to staff_root_path' do
-      expect(response).to redirect_to staff_login_path
-    end
+    before { patch :update, params: { staff_member: staff_member_params } }
+    it { subject }
   end
 end
 
 shared_examples 'a staff_member whose session is time out' do
   let(:staff_member) { create(:staff_member) }
-
+  let(:staff_member_params) { attributes_for(:staff_member, email: Faker::Internet.email) }
   before do
     session[:staff_member_id] = staff_member.id
     session[:last_access_time] = Staff::Base::TIMEOUT.ago.advance(seconds: -1)
   end
+  subject do
+    expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
+    expect(response).to redirect_to staff_login_path
+  end
 
   describe 'GET #show' do
     before { get :show }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to staff_root_path' do
-      expect(response).to redirect_to staff_login_path
-    end
+    it { subject }
   end
 
   describe 'GET #edit' do
     before { get :edit }
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to staff_root_path' do
-      expect(response).to redirect_to staff_login_path
-    end
+    it { subject }
   end
 
   describe 'PATCH #update' do
-    let(:staff_member_params) { attributes_for(:staff_member, email: 'test@test.com') }
-
-    before do
-      patch :update, params: { staff_member: staff_member_params }
-    end
-
-    it 'shows alert flash' do
-      expect(flash[:alert]).to eq 'セッションがタイムアウトしました'
-    end
-
-    it 'redirects to staff_root_path' do
-      expect(response).to redirect_to staff_login_path
-    end
+    before { patch :update, params: { staff_member: staff_member_params } }
+    it { subject }
   end
 end
 
 shared_examples 'a staff_member who does not active' do
+  let(:staff_member_params) { attributes_for(:staff_member, email: Faker::Internet.email) }
+  before do
+    session[:staff_member_id] = staff_member.id
+    session[:last_access_time] = 1.second.ago
+  end
+  subject do
+    expect(flash[:alert]).to eq 'アカウントが無効です'
+    expect(response).to redirect_to staff_root_path
+  end
+
   context 'when account is suspended' do
     let(:staff_member) { create(:staff_member, suspended: true) }
 
-    before do
-      session[:staff_member_id] = staff_member.id
-      session[:last_access_time] = 1.second.ago
-    end
-
     describe 'GET #show' do
       before { get :show }
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      it { subject }
     end
 
     describe 'GET #edit' do
       before { get :edit }
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      it { subject }
     end
 
     describe 'PATCH #update' do
-      let(:staff_member_params) { attributes_for(:staff_member, email: 'test@test.com') }
-
-      before do
-        patch :update, params: { staff_member: staff_member_params }
-      end
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      before { patch :update, params: { staff_member: staff_member_params } }
+      it { subject }
     end
   end
 
   context 'when start_date is after today' do
     let(:staff_member) { create(:staff_member, start_date: Time.zone.tomorrow, end_date: nil) }
 
-    before do
-      session[:staff_member_id] = staff_member.id
-      session[:last_access_time] = 1.second.ago
-    end
-
     describe 'GET #show' do
       before { get :show }
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      it { subject }
     end
 
     describe 'GET #edit' do
       before { get :edit }
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      it { subject }
     end
 
     describe 'PATCH #update' do
-      let(:staff_member_params) { attributes_for(:staff_member, email: 'test@test.com') }
-
-      before do
-        patch :update, params: { staff_member: staff_member_params }
-      end
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      before { patch :update, params: { staff_member: staff_member_params } }
+      it { subject }
     end
   end
 
@@ -196,49 +106,19 @@ shared_examples 'a staff_member who does not active' do
       staff_member
     end
 
-    before do
-      session[:staff_member_id] = staff_member.id
-      session[:last_access_time] = 1.second.ago
-    end
-
     describe 'GET #show' do
       before { get :show }
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      it { subject }
     end
 
     describe 'GET #edit' do
       before { get :edit }
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      it { subject }
     end
 
     describe 'PATCH #update' do
-      let(:staff_member_params) { attributes_for(:staff_member, email: 'test@test.com') }
-
-      before do
-        patch :update, params: { staff_member: staff_member_params }
-      end
-
-      it 'shows alert flash' do
-        expect(flash[:alert]).to eq 'アカウントが無効です'
-      end
-
-      it 'redirects to staff_root_path' do
-        expect(response).to redirect_to staff_root_path
-      end
+      before { patch :update, params: { staff_member: staff_member_params } }
+      it { subject }
     end
   end
 end
