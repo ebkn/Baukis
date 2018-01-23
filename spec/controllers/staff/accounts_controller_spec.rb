d5a6585cd582b190feed77925b45d1d6eeff290c
ebkn12 <ktennis.mqekr12@gmail.com>
2018-01-23 21:52:19 +0900

    refactoring staff/accounts_controller spec

--- spec/controllers/staff/accounts_controller_spec.rb
+++ spec/controllers/staff/accounts_controller_spec.rb
@@ -8,7 +8,6 @@ end
 
 describe Staff::AccountsController, type: :controller do
   let(:staff_member) { create(:staff_member) }
-
   before do
     session[:staff_member_id] = staff_member.id
     session[:last_access_time] = 1.second.ago
@@ -16,60 +15,35 @@ describe Staff::AccountsController, type: :controller do
 
   describe 'GET #show' do
     before { get :show }
-
-    it 'assigns the current_staff_member to @staff_member' do
-      expect(assigns(:staff_member)).to eq staff_member
-    end
-
-    it 'renders the :show template' do
-      expect(response).to render_template :show
-    end
+    it { expect(assigns(:staff_member)).to eq staff_member }
+    it { expect(response).to render_template :show }
   end
 
   describe 'GET #edit' do
     before { get :edit }
-
-    it 'assigns the current_staff_member to @staff_member' do
-      expect(assigns(:staff_member)).to eq staff_member
-    end
-
-    it 'renders the :edit template' do
-      expect(response).to render_template :edit
-    end
+    it { expect(assigns(:staff_member)).to eq staff_member }
+    it { expect(response).to render_template :edit }
   end
 
   describe 'PATCH #update' do
+    let(:new_email) { 'test@test.com' }
+    let(:staff_member_params) { attributes_for(:staff_member, email: new_email) }
+    let(:invalid_staff_member_params) { attributes_for(:staff_member, email: nil) }
     context 'when success' do
-      let(:staff_member_params) { attributes_for(:staff_member, email: 'test@test.com') }
-
-      before do
-        patch :update, params: { staff_member: staff_member_params }
+      before { patch :update, params: { staff_member: staff_member_params } }
+      it 'updates staff_member\'s email' do
+        staff_member.reload
+        expect(staff_member.email).to eq new_email
       end
-
-      it 'shows the notice flash' do
-        expect(flash[:notice]).to eq 'アカウント情報を更新しました'
-      end
-
-      it 'redirects to staff_account_path' do
-        expect(response).to redirect_to staff_account_path
-      end
+      it { expect(flash[:notice]).to eq 'アカウント情報を更新しました' }
+      it { expect(response).to redirect_to staff_account_path }
     end
 
     context 'when failure' do
       context 'when invalid parameter' do
-        let(:invalid_staff_member_params) { attributes_for(:staff_member, email: nil) }
-
-        before do
-          patch :update, params: { staff_member: invalid_staff_member_params }
-        end
-
-        it 'shows the alert flash' do
-          expect(flash[:alert]).to eq 'アカウント情報の更新に失敗しました'
-        end
-
-        it 'renders the :edit template' do
-          expect(response).to render_template :edit
-        end
+        before { patch :update, params: { staff_member: invalid_staff_member_params } }
+        it { expect(flash[:alert]).to eq 'アカウント情報の更新に失敗しました' }
+        it { expect(response).to render_template :edit }
       end
 
       context 'when raise error' do
