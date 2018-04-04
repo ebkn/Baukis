0164f52b373acc9bbe7347aa4e2c86f04034f6cc
ebkn12 <ktennis.mqekr12@gmail.com>
2018-04-04 22:58:52 +0900

    feat: rails app:update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile +=
   %w[admin.css staff.css customer.css admin.js staff.js customer.js]
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
